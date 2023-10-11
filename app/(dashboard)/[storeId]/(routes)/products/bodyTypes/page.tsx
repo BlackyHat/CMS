3eb7fa39d39b9eebf8bfcdb9b43009c7c3b237df
Oleksandr P <goodwin009@gmail.com>
2023-10-11 13:38:21 +0300

    fix bodyTypes page container styles

--- app/(dashboard)/[storeId]/(routes)/products/bodyTypes/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/bodyTypes/page.tsx
@@ -19,7 +19,7 @@ const BodyTypesPage = async ({ params }: { params: { storeId: string } }) => {
   );
 
   return (
-    <div className="flex-col">
+    <div className="max-w-screen-2xl m-auto">
       <div className="flex-1 space-y-4 p-8 pt-0">
         <BodyTypeClient data={formattedBodyTypes} />
       </div>
