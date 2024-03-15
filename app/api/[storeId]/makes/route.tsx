b16c852f97c0ae264043df963b869b679824ad6a
Oleksandr P <goodwin009@gmail.com>
2024-03-15 14:46:57 +0200

    fix makes and regopns get requests

--- app/api/[storeId]/makes/route.tsx
+++ app/api/[storeId]/makes/route.tsx
@@ -1,51 +1,51 @@
-import prismadb from '@/lib/prismadb';
-import { UserRoles } from '@/types/enums';
-import { auth } from '@clerk/nextjs';
-import { NextResponse } from 'next/server';
+import prismadb from '@/lib/prismadb'
+import { UserRoles } from '@/types/enums'
+import { auth } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
 
 export async function POST(
   req: Request,
   { params }: { params: { storeId: string } }
 ) {
   try {
-    const { userId, sessionClaims } = auth();
-    const body = await req.json();
+    const { userId, sessionClaims } = auth()
+    const body = await req.json()
 
-    const { label } = body;
+    const { label } = body
 
     if (!userId) {
-      return new NextResponse('Unauthenticated', { status: 401 });
+      return new NextResponse('Unauthenticated', { status: 401 })
     }
     if (sessionClaims.role !== UserRoles.ADMIN) {
       return new NextResponse('Forbidden. Administrator rights are required.', {
         status: 403,
-      });
+      })
     }
 
     if (!label) {
-      return new NextResponse('Label is required', { status: 400 });
+      return new NextResponse('Label is required', { status: 400 })
     }
     if (!params.storeId) {
-      return new NextResponse('Store ID is required', { status: 400 });
+      return new NextResponse('Store ID is required', { status: 400 })
     }
 
     const storeByUserId = await prismadb.store.findFirst({
       where: { id: params.storeId, userId },
-    });
+    })
 
     if (!storeByUserId) {
-      return new NextResponse('Unauthorized', { status: 403 });
+      return new NextResponse('Unauthorized', { status: 403 })
     }
     const make = await prismadb.make.create({
       data: {
         label,
       },
-    });
+    })
 
-    return NextResponse.json(make);
+    return NextResponse.json(make)
   } catch (error) {
-    console.log('[MAKE_POST]', error);
-    return new NextResponse('Internal error', { status: 500 });
+    console.log('[MAKE_POST]', error)
+    return new NextResponse('Internal error', { status: 500 })
   }
 }
 export async function GET(
@@ -54,14 +54,17 @@ export async function GET(
 ) {
   try {
     if (!params.storeId) {
-      return new NextResponse('Store ID is required', { status: 400 });
+      return new NextResponse('Store ID is required', { status: 400 })
     }
 
-    const makes = await prismadb.make.findMany();
+    const makes = await prismadb.make.findMany({
+      include: { models: { orderBy: { label: 'asc' } } },
+      orderBy: { label: 'asc' },
+    })
 
-    return NextResponse.json(makes);
+    return NextResponse.json(makes)
   } catch (error) {
-    console.log('[MAKES_GET]', error);
-    return new NextResponse('Internal error', { status: 500 });
+    console.log('[MAKES_GET]', error)
+    return new NextResponse('Internal error', { status: 500 })
   }
 }
--- app/api/[storeId]/regions/route.tsx
+++ app/api/[storeId]/regions/route.tsx
@@ -1,48 +1,48 @@
-import prismadb from '@/lib/prismadb';
-import { UserRoles } from '@/types/enums';
-import { auth } from '@clerk/nextjs';
-import { NextResponse } from 'next/server';
+import prismadb from '@/lib/prismadb'
+import { UserRoles } from '@/types/enums'
+import { auth } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
 
 export async function POST(
   req: Request,
   { params }: { params: { storeId: string } }
 ) {
   try {
-    const { userId, sessionClaims } = auth();
-    const body = await req.json();
+    const { userId, sessionClaims } = auth()
+    const body = await req.json()
 
-    const { name } = body;
+    const { name } = body
 
     if (!userId) {
-      return new NextResponse('Unauthenticated', { status: 401 });
+      return new NextResponse('Unauthenticated', { status: 401 })
     }
     if (sessionClaims.role !== UserRoles.ADMIN) {
       return new NextResponse('Forbidden. Administrator rights are required.', {
         status: 403,
-      });
+      })
     }
 
     if (!name) {
-      return new NextResponse('Region name is required', { status: 400 });
+      return new NextResponse('Region name is required', { status: 400 })
     }
 
     const storeByUserId = await prismadb.store.findFirst({
       where: { id: params.storeId, userId },
-    });
+    })
 
     if (!storeByUserId) {
-      return new NextResponse('Unauthorized', { status: 403 });
+      return new NextResponse('Unauthorized', { status: 403 })
     }
     const region = await prismadb.region.create({
       data: {
         name,
       },
-    });
+    })
 
-    return NextResponse.json(region);
+    return NextResponse.json(region)
   } catch (error) {
-    console.log('[REGION_POST]', error);
-    return new NextResponse('Internal error', { status: 500 });
+    console.log('[REGION_POST]', error)
+    return new NextResponse('Internal error', { status: 500 })
   }
 }
 export async function GET(
@@ -51,14 +51,19 @@ export async function GET(
 ) {
   try {
     if (!params.storeId) {
-      return new NextResponse('Store ID is required', { status: 400 });
+      return new NextResponse('Store ID is required', { status: 400 })
     }
 
-    const regions = await prismadb.region.findMany();
+    const regions = await prismadb.region.findMany({
+      include: { cities: { orderBy: { name: 'asc' } } },
+      orderBy: {
+        updatedAt: 'asc',
+      },
+    })
 
-    return NextResponse.json(regions);
+    return NextResponse.json(regions)
   } catch (error) {
-    console.log('[REGIONS_GET]', error);
-    return new NextResponse('Internal error', { status: 500 });
+    console.log('[REGIONS_GET]', error)
+    return new NextResponse('Internal error', { status: 500 })
   }
 }
